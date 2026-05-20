510f5a1fc6e70d796ef9468335d0ab131dbff1b3
ABDULHADI KEZZA <hadikezzhe@gmail.com>
2026-05-20 14:21:17 +0300

    learned (use state ) add create component for it
    I learned how to use (use state ) and i understood  how its works then i Added MyButton component with toggle functionality

---
+++ src/MyButton.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+export default function MyButton()
+{
+    console.log("render");
+
+    const [name , setName] = useState("Ahmed");
+
+    
+    function buttonClicked(){
+        if(name==="Ahmed"){
+            setName("Hadi");
+        }else{
+            setName("Ahmed");
+        }
+    }
+
+    return(
+        <div>
+            <button onClick={buttonClicked}>Click Me</button>
+            <h1>{name}</h1>
+        </div>
+    );
+}
