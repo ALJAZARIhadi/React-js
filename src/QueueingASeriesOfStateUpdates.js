661b768075734f655f95a59f75343997d7a7c2d7
ABDULHADI KEZZA <hadikezzhe@gmail.com>
2026-05-27 02:23:49 +0300

    Correct state update logic in Counter component
    Fix button click handler to correctly queue state updates.

--- src/QueueingASeriesOfStateUpdates.js
+++ src/QueueingASeriesOfStateUpdates.js
@@ -1,1 +1,21 @@
+import { useState } from 'react';
 
+export default function Counter() {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <>
+      <h1>{number}</h1>
+      <button onClick={() => {
+        setNumber(number +1);
+      setNumber((e)=>{
+          return e +1
+        });
+       setNumber((e)=>{
+          return e +1
+        });
+        
+      }}>+3</button>
+    </>
+  )
+}
