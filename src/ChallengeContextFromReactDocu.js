278a3f19dfce4c3bb00d7f6394b57ba728ace75b
ABDULHADI KEZZA <hadikezzhe@gmail.com>
2026-08-24 10:43:18 +0300

    Implement image context and dynamic image sizing

--- src/ChallengeContextFromReactDocu.js
+++ src/ChallengeContextFromReactDocu.js
@@ -1,1 +1,64 @@
+//Context.js file
+import { createContext } from 'react';
 
+export const ImageContext = createContext();
+
+//App.js
+
+import { useState } from 'react';
+import { places } from './data.js';
+import { getImageUrl } from './utils.js';
+import { ImageContext } from './Context.js';
+
+export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+
+  return (
+    <ImageContext.Provider value={imageSize}>
+      <label>
+        <input
+          type="checkbox"
+          checked={isLarge}
+          onChange={e => setIsLarge(e.target.checked)}
+        />
+        Use large images
+      </label>
+      <hr />
+      <List />
+    </ImageContext.Provider>
+  );
+}
+
+function List() {
+  const listItems = places.map(place =>
+    <li key={place.id}>
+      <Place place={place} />
+    </li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function Place({ place }) {
+  return (
+    <>
+      <PlaceImage place={place} />
+      <p>
+        <b>{place.name}</b>
+        {': ' + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage({ place }) {
+  const imageSize = useContext(ImageContext); 
+  return (
+    <img
+      src={getImageUrl(place)}
+      alt={place.name}
+      width={imageSize}
+      height={imageSize}
+    />
+  );
+}
